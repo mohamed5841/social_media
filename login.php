0206a59670fbecd7926e6c7c01b82e94eb59c11f
mohamed abdelhamid <hhoms8978@gmail.com>
2023-07-17 18:02:25 +0300

    make php login

---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // قراءة الملف JSON والتحقق من اسم المستخدم وكلمة المرور
+    $usersFile = file_get_contents("users.json");
+    $usersData = json_decode($usersFile, true);
+
+    $validUser = false;
+    foreach ($usersData as $user) {
+        if ($user['username'] === $username && $user['password'] === $password) {
+            $validUser = true;
+            break;
+        }
+    }
+
+    if ($validUser) {
+        echo "تم تسجيل الدخول بنجاح";
+        // قم بتوجيه المستخدم إلى صفحة الوجهة بعد تسجيل الدخول بنجاح
+        // يمكنك استبدال "welcome.php" بالصفحة التي ترغب في توجيه المستخدم إليها
+       // header("Location: welcome.php");
+        //exit();
+    } else {
+        $errorMessage = "invalid username or password";
+    }
+}
+?>
